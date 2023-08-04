255e192c9fc0403c6e1de7721b8a108a8d064ab7
grahampaulcook <grahampaulcook@users.noreply.github.com>
2023-08-04 07:22:28 +0100

    ETFE-1357 Add additional tests for connectors

---
+++ it/connectors/GetPackagingTypesConnectorISpec.scala
@@ -0,0 +1,104 @@
+package connectors
+
+import com.github.tomakehurst.wiremock.client.WireMock._
+import com.github.tomakehurst.wiremock.http.Fault
+import connectors.packagingTypes.GetPackagingTypesConnector
+import generators.ModelGenerators
+import models.requests.PackagingTypesRequest
+import models.response.referenceData.PackagingTypesResponse
+import models.{JsonValidationError, UnexpectedDownstreamResponseError}
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.{EitherValues, OptionValues}
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.Application
+import play.api.http.Status.{INTERNAL_SERVER_ERROR, OK}
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.{JsArray, JsString, Json}
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class GetPackagingTypesConnectorISpec
+  extends AnyFreeSpec
+    with WireMockHelper
+    with ScalaFutures
+    with Matchers
+    with IntegrationPatience
+    with EitherValues
+    with OptionValues
+    with MockitoSugar
+    with ModelGenerators {
+
+  implicit private lazy val hc: HeaderCarrier = HeaderCarrier()
+
+  private lazy val app: Application =
+    new GuiceApplicationBuilder()
+      .configure(
+        "microservice.services.reference-data.port" -> server.port
+      )
+      .build()
+
+  private lazy val connector: GetPackagingTypesConnector = app.injector.instanceOf[GetPackagingTypesConnector]
+
+  ".check" - {
+
+    val url = "/emcs-tfe-reference-data/oracle/packaging-types"
+    val request = PackagingTypesRequest(Seq("TN"))
+    val requestJson = JsArray(Seq(JsString("TN")))
+
+    "must return a response model when the server responds OK" in {
+
+      server.stubFor(
+        post(urlEqualTo(url))
+          .withRequestBody(equalToJson(Json.stringify(requestJson)))
+          .willReturn(aResponse().withStatus(OK).withBody(Json.stringify(Json.obj(
+            "TN" -> "Tin"
+          ))))
+      )
+
+      connector.getPackagingTypes(request).futureValue mustBe Right(PackagingTypesResponse(data = Map(
+        "TN" -> "Tin"
+      )))
+    }
+
+    "must fail when the server responds with any other status" in {
+
+      server.stubFor(
+        post(urlEqualTo(url))
+          .withRequestBody(equalToJson(Json.stringify(requestJson)))
+          .willReturn(aResponse().withStatus(INTERNAL_SERVER_ERROR))
+      )
+
+      connector.getPackagingTypes(request).futureValue mustBe Left(UnexpectedDownstreamResponseError)
+    }
+
+    "must fail when the server responds with a body that can't be parsed to the expected response model" in {
+
+      server.stubFor(
+        post(urlEqualTo(url))
+          .withRequestBody(equalToJson(Json.stringify(requestJson)))
+          .willReturn(aResponse().withStatus(OK).withBody(Json.stringify(Json.obj(
+            "24029000" -> JsArray(Seq(Json.obj(
+              "cnCodeDescription" -> "Cigars, cheroots, cigarillos and cigarettes not containing tobacco",
+              "unitOfMeasureCode" -> 1
+            )))
+          ))))
+      )
+
+      connector.getPackagingTypes(request).futureValue mustBe Left(JsonValidationError)
+    }
+
+    "must fail when the connection fails" in {
+
+      server.stubFor(
+        post(urlEqualTo(url))
+          .withRequestBody(equalToJson(Json.stringify(Json.toJson(request))))
+          .willReturn(aResponse().withFault(Fault.RANDOM_DATA_THEN_CLOSE))
+      )
+
+      connector.getPackagingTypes(request).futureValue mustBe Left(UnexpectedDownstreamResponseError)
+    }
+  }
+}
