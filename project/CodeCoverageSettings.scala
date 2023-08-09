c38ef12a0ca1aaadce657507d83d9b416431c9b7
james-work-account <31282758+james-work-account@users.noreply.github.com>
2023-08-09 10:53:36 +0100

    [ETFE-1588] use CodeCoverageSettings.scala file rather than inline settings

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -6,17 +6,24 @@ object CodeCoverageSettings {
   private val excludedPackages: Seq[String] = Seq(
     "<empty>",
     "Reverse.*",
+    ".*handlers.*",
+    ".*components.*",
     "uk.gov.hmrc.BuildInfo",
     "app.*",
     "prod.*",
     ".*Routes.*",
+    ".*viewmodels.govuk.*",
     "testOnly.*",
+    ".*views.html.template.*",
+    ".*pages.*",
+    ".*featureswitch.*",
+    ".*forms.mappings.*",
     "testOnlyDoNotUseInAppConf.*"
   )
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 100,
+    ScoverageKeys.coverageMinimumStmtTotal := 95,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
