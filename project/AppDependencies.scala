5b58f98b8747cb2525204ba60768565b85a1a51a
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2024-01-08 12:30:29 +0000

    [ETFE-2985] - Updated test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,10 +5,10 @@ object AppDependencies {
   import play.core.PlayVersion
 
   val playSuffix            = "-play-30"
-  val scalatestVersion      = "3.2.15"
+  val scalatestVersion      = "3.2.17"
   val hmrcBootstrapVersion  = "8.4.0"
-  val hmrcMongoVersion      = "1.6.0"
-  val catsCoreVersion       = "2.3.1"
+  val hmrcMongoVersion      = "1.7.0"
+  val catsCoreVersion       = "2.10.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
@@ -20,17 +20,17 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% s"bootstrap-test$playSuffix"         % hmrcBootstrapVersion,
+    "uk.gov.hmrc"             %% s"bootstrap-test$playSuffix"          % hmrcBootstrapVersion,
     "org.scalatest"           %%  "scalatest"                         % scalatestVersion,
     "org.scalatestplus"       %%  "scalacheck-1-17"                   % s"$scalatestVersion.0",
-    "org.scalatestplus"       %%  "mockito-4-6"                       % s"$scalatestVersion.0",
+    "org.scalatestplus"       %%  "mockito-5-8"                       % s"$scalatestVersion.0",
     "org.scalatestplus.play"  %%  "scalatestplus-play"                % "5.1.0",
     "org.scalamock"           %%  "scalamock"                         % "5.2.0",
     "org.pegdown"             %   "pegdown"                           % "1.6.0",
-    "org.jsoup"               %   "jsoup"                             % "1.15.4",
+    "org.jsoup"               %   "jsoup"                             % "1.17.2",
     "org.playframework"       %%  "play-test"                         % PlayVersion.current,
-    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test$playSuffix"        % hmrcMongoVersion,
-    "com.vladsch.flexmark"    %   "flexmark-all"                      % "0.62.2"
+    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test$playSuffix"         % hmrcMongoVersion,
+    "com.vladsch.flexmark"    %   "flexmark-all"                        % "0.64.8"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
