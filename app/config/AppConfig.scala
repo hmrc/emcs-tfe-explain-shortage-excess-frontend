f7839e825bf1e320a414129d069814eb506144c1
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2024-10-28 10:33:48 +0000

    [ETFE-4638] Remove beta banner, point to generic feedbakc survey and major bump (#74)

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -18,8 +18,6 @@ package config
 
 import featureswitch.core.config.FeatureSwitching
 import play.api.Configuration
-import play.api.mvc.RequestHeader
-import uk.gov.hmrc.play.bootstrap.binders.SafeRedirectUrl
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 import javax.inject.{Inject, Singleton}
@@ -47,9 +45,7 @@ class AppConfig @Inject()(servicesConfig: ServicesConfig, configuration: Configu
 
   private lazy val feedbackFrontendHost: String = configuration.get[String]("feedback-frontend.host")
 
-  lazy val feedbackFrontendSurveyUrl: String = s"$feedbackFrontendHost/feedback/$deskproName/beta"
-
-  private lazy val contactHost = configuration.get[String]("contact-frontend.host")
+  lazy val feedbackFrontendSurveyUrl: String = s"$feedbackFrontendHost/feedback/$deskproName"
 
   lazy val loginUrl: String = configuration.get[String]("urls.login")
 
@@ -91,9 +87,6 @@ class AppConfig @Inject()(servicesConfig: ServicesConfig, configuration: Configu
     configuration.get[String]("urls.emcsTfeMovementDetails").replace(":ern", ern).replace(":arc", arc)
 
   def emcsMovementsUrl(ern: String): String = configuration.get[String]("urls.emcsTfeMovements").replace(":ern", ern)
-
-  def betaBannerFeedbackUrl(implicit request: RequestHeader): String =
-    s"$contactHost/contact/beta-feedback?service=$deskproName&backUrl=${SafeRedirectUrl(host + request.uri).encodedUrl}"
 
   private def referenceDataService: String = servicesConfig.baseUrl("emcs-tfe-reference-data")
 
